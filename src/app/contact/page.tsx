d8655988f0107452b6ec386a0c9af6a8a6587c31
aryanagfundfina <alemantrixaryan@gmail.com>
2025-07-24 10:27:25 +0530

    contact is also done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,9 +17,7 @@ export default function ContactPage() {
   return (
     <>
       <ContactHero />
-      <ContactInfo />
       <ContactForm />
-      <OfficeDetails />
       <CTASection />
     </>
   );
